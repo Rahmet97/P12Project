e0f3588fbfb450b2e5da328f17f6f727ad554083
user <user@linux.kali>
2023-06-02 21:37:00 +0500

    User register done

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password == password_confirm:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists! Please enter another username.')
+                return redirect('register')
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists! Please enter another email.')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    username=username,
+                    password=password
+                )
+                user.save()
+                return redirect('/')
+        else:
+            messages.error(request, 'Passwords are not same')
+            return redirect('register')
+    return render(request, 'register.html')
--- main/models.py
+++ main/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     title = models.CharField(max_length=50)
     price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='pics')
     description = models.TextField()
 
