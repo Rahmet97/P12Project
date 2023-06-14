f35d85444943695ce0b4e7617934bd0133468653
user <user@linux.kali>
2023-06-14 20:27:30 +0500

    Static.

--- root/settings.py
+++ root/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = '/accounts/login'
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
