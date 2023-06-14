518eb6ec5bd6a0e2ffe72c598a8ef6157372b5e5
user <user@linux.kali>
2023-06-14 20:31:59 +0500

    Static..

--- root/urls.py
+++ root/urls.py
@@ -31,3 +31,4 @@ urlpatterns += i18n_patterns(
     path("i18n/", include("django.conf.urls.i18n")),
 )
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
